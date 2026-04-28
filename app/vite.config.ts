4f4357db5be521374a6159420af0220f0e791f45
Yahavi <yahavi1212@gmail.com>
2026-04-28 17:19:42 +0530

    chore: cleanup duplicate deps, fix vite base path + config

--- app/vite.config.ts
+++ app/vite.config.ts
@@ -3,10 +3,11 @@ import react from "@vitejs/plugin-react"
 import { defineConfig } from "vite"
 
 export default defineConfig({
-  base: './',
+  base: '/',
   plugins: [react()],
   server: {
-    port: 3000,
+    host: '0.0.0.0',
+    port: Number(process.env.PORT) || 3000,
   },
   resolve: {
     alias: {
